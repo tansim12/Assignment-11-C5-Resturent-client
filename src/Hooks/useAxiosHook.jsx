0ff32d2adf0bd33e9f0fd4d28aeca21c6aa80499
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-11-07 02:24:35 +0600

    no product found page my order route is implement

---
+++ src/Hooks/useAxiosHook.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+  baseURL:""
+})
+const useAxiosHook = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default useAxiosHook;
--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -3,16 +3,15 @@ import axios from "axios";
 import useAuthContext from "../../Hooks/useAuthContext";
 import MyOrderCard from "./MyOrderCard";
 import LoadingSkeleton from "../LoadingSkeleton/LoadingSkeleton";
-// import { useEffect, useState } from "react";
+import NoProducts from "../../Components/NoProducts/NoProducts";
+import Lottie from "lottie-react";
+import noFound from "../../assets/nodataFound.json"
+
 
 const MyOrder = () => {
   const { user } = useAuthContext();
-  // const [order , setOrder] = useState([])
 
-  // useEffect(as()=>{
-
-  // })
-  const { data, isLoading, refetch, isFetched } = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ["myOrder", user],
     queryFn: async () => {
       try {
@@ -26,6 +25,7 @@ const MyOrder = () => {
       }
     },
   });
+  console.log(data);
 
   if (isLoading) {
     return <LoadingSkeleton></LoadingSkeleton>;
@@ -33,13 +33,22 @@ const MyOrder = () => {
 
   return (
     <div>
+      
+        {data.length === 0 && (
+          <div className="mt-20 p-5">
+            <NoProducts></NoProducts>
+            <div className="flex justify-center items-center my-10">
+                <Lottie animationData={noFound} className="w-full md:w-1/2"></Lottie>
+            </div>
+          </div>
+        )}
+      
       <div>
         {data?.map((item) => (
           <MyOrderCard
             key={item?._id}
             item={item}
             refetch={refetch}
-            isFetched={isFetched}
           ></MyOrderCard>
         ))}
       </div>
